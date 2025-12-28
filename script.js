4f501523455e281a269f82c36a27ff387e4290bc
kostas900402 <nascoskostas@gmail.com>
2025-12-28 10:32:00 +0100

    klart, js fungerar, css uppdaterad

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const cells = document.querySelectorAll('.cell');
 const statusText = document.getElementById('status');
-const resetButton = document.getElementById('reset-btn');
+const resetBtn = document.getElementById('reset-btn');
 
+let board = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let gameActive = true;
-let board = ['', '', '', '', '', '', '', '', ''];
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 function handleCellClick(e) {
-    const clickedCell = e.target;
-    const clickedIndex = parseInt(clickedCell.getAttribute('data-index'));
-
-    if (board[clickedIndex] !== '' || !gameActive) {
+    const cell = e.target;
+    const index = cell.getAttribute('data-index');
+    
+    if (board[index] !== '' || !gameActive) {
         return;
     }
-    updateCell(clickedCell, clickedIndex);
+    
+    updateCell(cell, index);
     checkWinner();
 }
 
@@ -21,18 +33,63 @@ function updateCell(cell, index) {
     board[index] = currentPlayer;
     cell.textContent = currentPlayer;
     cell.disabled = true;
-}   
+}
 
 function checkWinner() {
+    let roundWon = false;
+    
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        
+        const a = board[condition[0]];
+        const b = board[condition[1]];
+        const c = board[condition[2]];
+        
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+    
+    if (roundWon) {
+        statusText.textContent = `Spelare ${currentPlayer} vinner!`;
+        gameActive = false;
+        return;
+    }
+    
+    if (!board.includes('')) {
+        statusText.textContent = `Oavgjort!`;
+        gameActive = false;
+        return;
+    }
+    
     changePlayer();
-
-
 }
 
 function changePlayer() {
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    statusText.textContent = `Spelare ${currentPlayer}s tur`;
+    statusText.textContent = `Spelare ${currentPlayer}:s tur`;
 }
 
-cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+function resetGame() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    currentPlayer = 'X';
+    gameActive = true;
+    
+    statusText.textContent = `Spelare ${currentPlayer}:s tur`;
+    
+    cells.forEach(cell => {
+        cell.textContent = '';
+        cell.disabled = false;
+    });
+}
 
+cells.forEach(cell => {
+    cell.addEventListener('click', handleCellClick);
+});
+
+resetBtn.addEventListener('click', resetGame);
